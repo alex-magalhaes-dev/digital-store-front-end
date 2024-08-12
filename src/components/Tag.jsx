d0070ac023df7d892165203b2061878a5400a622
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-08-12 17:35:29 -0300

    update(change Tag component)

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,4 +1,4 @@
-export default function CollectionTag({ text }) {
+export default function Tag({ text }) {
    return (
       <div className="bg-[#E7FF86] font-bold text-sm tracking-wide rounded-full px-4 py-1 uppercase">
          <span>
